9ea97bfd81b84d055255113a28bbbbaf713b563e
TheeDee6 <officialtheodora20@gmail.com>
2024-11-19 17:16:19 +0000

    Updated schema

--- models/vocabulary.js
+++ models/vocabulary.js
@@ -55,6 +55,46 @@ const VocabularySchema = new Schema(
             type: String,
             required: [true, "Meaning is required"],
         },
+        partOfSpeech: {
+            type: String,
+            required: [true, "Part of speech is required"],
+            enum: [
+                "noun",
+                "verb",
+                "adjective",
+                "adverb",
+                "pronoun",
+                "preposition",
+                "conjunction",
+                "interjection",
+                "other",
+            ],
+        },
+        pronunciation: {
+            type: String,
+            trim: true,
+            default: "",
+        },
+        synonyms: {
+            type: [String], // Array of synonyms
+            default: [],
+            validate: {
+                validator: function (synonyms) {
+                    return Array.isArray(synonyms) && synonyms.every((s) => typeof s === "string");
+                },
+                message: "Synonyms must be an array of strings.",
+            },
+        },
+        antonyms: {
+            type: [String], // Array of antonyms
+            default: [],
+            validate: {
+                validator: function (antonyms) {
+                    return Array.isArray(antonyms) && antonyms.every((a) => typeof a === "string");
+                },
+                message: "Antonyms must be an array of strings.",
+            },
+        },
         exampleSentence: {
             type: String,
             required: [true, "Example sentence is required"],
